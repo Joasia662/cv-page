8122dbe906bbd6ef5017651fd606b9d842c01a3c
Joanna Cepuch <asiacepuch1@gmail.com>
2023-11-28 23:51:48 +0100

    added hand-writting library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Vara from 'vara';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cv-page';
 
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+  var vara = new Vara("#vara-container","https://cdn.jsdelivr.net/npm/vara@1.4.0/fonts/Satisfy/SatisfySL.json",[{
+    text:"Joanna Cepuch"
+}],{
+  fontSize:140
+});
+  }
+
 
 }
+
+
+//fonts/Pacifico
+//fonts/Shadows-Into-Light
+//fonts/Parisienne
+//fonts/Satisfy
