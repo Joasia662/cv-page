c462cd4ef1709c218b000293ef1fe56b5e372f4d
Joanna Cepuch <asiacepuch1@gmail.com>
2024-07-17 20:57:47 +0200

    fixed laptop size

--- src/app/project-cards/project-cards.component.ts
+++ src/app/project-cards/project-cards.component.ts
@@ -5,6 +5,77 @@ import { Component } from '@angular/core';
   templateUrl: './project-cards.component.html',
   styleUrls: ['./project-cards.component.scss']
 })
+
+/*
+projects = [
+    {
+      title: '',
+      imgPath: '',
+      desc: '',
+      repo: '',
+      host: '',
+      tech: {
+        angular: {
+          isPresent: false,
+          isInProgress: false,
+          version: '0.0'
+        },
+        NgRx: {
+          isPresent: false,
+          isInProgress: false,
+        },
+        Jasmine:{
+          isPresent: false,
+          isInProgress: false,
+        },
+        Matrials:{
+          isPresent: false,
+          isInProgress: false,
+        }
+      }
+    }
+  ]
+*/
 export class ProjectCardsComponent {
+
+  constructor() {
+    this.selectedItem = this.projects[0]
+  }
+
+  selectedItem: any = {}
+  projects = [
+    {
+      title: 'Weather Forecast',
+      imgPath: '../assets/WeatherForecastScreen.png',
+      desc: "A Single Page Application that display a 5-day forecast for a city by name. ",
+      isApiMine: false,
+      apiLink:'https://openweathermap.org/forecast5',
+      repo: 'https://github.com/Joasia662/Weather-Forecast',
+      host: '',
+      tech: {
+        angular: {
+          isPresent: false,
+          isInProgress: false,
+          version: '0.0'
+        },
+        NgRx: {
+          isPresent: false,
+          isInProgress: false,
+        },
+        Jasmine: {
+          isPresent: false,
+          isInProgress: false,
+        },
+        Matrials: {
+          isPresent: false,
+          isInProgress: false,
+        }
+      }
+    }
+  ]
+  
+  changeSelectedItem(){
+
+  }
 
 }
