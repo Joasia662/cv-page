04495bdd7808c8f19fec800f5ff6a29220a39b72
Joanna Cepuch <asiacepuch1@gmail.com>
2024-07-17 21:35:54 +0200

    added technologies to the projects

--- src/app/project-cards/project-cards.component.ts
+++ src/app/project-cards/project-cards.component.ts
@@ -20,15 +20,15 @@ projects = [
           isInProgress: false,
           version: '0.0'
         },
-        NgRx: {
+        ngRx: {
           isPresent: false,
           isInProgress: false,
         },
-        Jasmine:{
+        jasmine:{
           isPresent: false,
           isInProgress: false,
         },
-        Matrials:{
+        materials:{
           isPresent: false,
           isInProgress: false,
         }
@@ -39,42 +39,89 @@ projects = [
 export class ProjectCardsComponent {
 
   constructor() {
-    this.selectedItem = this.projects[0]
+    this.selectedIndex = 0;
   }
 
-  selectedItem: any = {}
+  selectedIndex: number
   projects = [
     {
       title: 'Weather Forecast',
       imgPath: '../assets/WeatherForecastScreen.png',
       desc: "A Single Page Application that display a 5-day forecast for a city by name. ",
       isApiMine: false,
-      apiLink:'https://openweathermap.org/forecast5',
+      apiLink: 'https://openweathermap.org/forecast5',
       repo: 'https://github.com/Joasia662/Weather-Forecast',
       host: '',
       tech: {
         angular: {
-          isPresent: false,
+          isPresent: true,
           isInProgress: false,
           version: '0.0'
         },
-        NgRx: {
+        ngRx: {
+          isPresent: true,
+          isInProgress: false,
+        },
+        jasmine: {
+          isPresent: true,
+          isInProgress: false,
+        },
+        materials: {
+          isPresent: true,
+          isInProgress: false,
+        },
+        nodeJS: {
+          isPresent: false,
+          isInProgress: false,
+        }
+      }
+    },
+    {
+      title: 'Single Page Cook Book',
+      imgPath: '../assets/RecipeManager.png',
+      desc: `The application manage recipes. It allows users to create, edit, remove and list recipes. <b>It is currently in progress.</b><br>
+       In next version all mentioned functionalities and state managment ngXs will be implemented.<br>
+       This app currently uses mocks but in the near future it I will create an API for it in NodeJS`,
+      isApiMine: true,
+      repo: 'https://github.com/Joasia662/Recipes-Menager',
+      host: '',
+      tech: {
+        angular: {
+          isPresent: true,
+          isInProgress: false,
+          version: '0.0'
+        },
+        ngRx: {
           isPresent: false,
           isInProgress: false,
         },
-        Jasmine: {
+        ngXs: {
+          isPresent: true,
+          isInProgress: true,
+        },
+        nodeJS: {
+          isPresent: true,
+          isInProgress: false,
+        },
+        jasmine: {
           isPresent: false,
           isInProgress: false,
         },
-        Matrials: {
-          isPresent: false,
+        materials: {
+          isPresent: true,
           isInProgress: false,
         }
       }
     }
   ]
-  
-  changeSelectedItem(){
+
+  changeSelectedItem(actionName: string) {
+    if (actionName === 'next') {
+      this.selectedIndex + 1 === this.projects.length ? this.selectedIndex = 0 : this.selectedIndex++
+    }
+    else {
+      this.selectedIndex === 0 ? this.selectedIndex = this.projects.length - 1 : this.selectedIndex--
+    }
 
   }
 
